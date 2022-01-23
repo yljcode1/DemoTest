93f3a2d58bf71a883e466301336e72b9917b0a62
yljcode1 <aaa2826455837@gmail.com>
2022-01-23 11:23:20 +0800

    es-修改文档

--- Es/es-java/src/main/java/com/yao/esjava/EsTest_Doc_Update.java
+++ Es/es-java/src/main/java/com/yao/esjava/EsTest_Doc_Update.java
@@ -1,11 +1,12 @@
 package com.yao.esjava;
 
 import com.alibaba.fastjson.JSON;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -13,22 +14,19 @@ import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.IOException;
 
-/**
- * 文档的创建
- */
-public class EsTest_Doc_Insert {
+public class EsTest_Doc_Update {
     public static void main(String[] args) throws IOException {
         // 创建客户端
         RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("localhost", 9200, "http"))
         );
         // 插入数据
-        IndexRequest request = new IndexRequest();
-        request.index("user").id("1001");
         // 向ES插入数据，必须数据转换为JSON格式
+        UpdateRequest request = new UpdateRequest();
+        request.index("user").id("1001");
         String jsonString = JSON.toJSONString(User.builder().age(12).name("zhangsan").sex("男").build());
-        request.source(jsonString, XContentType.JSON);
-        IndexResponse response = restHighLevelClient.index(request, RequestOptions.DEFAULT);
+        request.doc(jsonString, XContentType.JSON);
+        UpdateResponse response = restHighLevelClient.update(request, RequestOptions.DEFAULT);
         DocWriteResponse.Result result = response.getResult();
         restHighLevelClient.close();
     }
