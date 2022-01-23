4f4675514c39598b237e610883664876e90d0b61
yljcode1 <aaa2826455837@gmail.com>
2022-01-23 11:31:59 +0800

    es-删除文档

---
+++ Es/es-java/src/main/java/com/yao/esjava/ESTest_Doc_Delete.java
@@ -0,0 +1,26 @@
+package com.yao.esjava;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.IOException;
+
+public class ESTest_Doc_Delete {
+    public static void main(String[] args) throws IOException {
+        RestHighLevelClient esClient = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        // 查询数据
+        DeleteRequest request = new DeleteRequest();
+        request.index("user").id("1001");
+        DeleteResponse response = esClient.delete(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getResult());
+        esClient.close();
+    }
+}
