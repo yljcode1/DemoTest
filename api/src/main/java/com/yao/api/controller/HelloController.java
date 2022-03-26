df9f65bf350e1befa6aff2c3d475a9776f90af20
yljcode1 <aaa2826455837@gmail.com>
2022-03-26 14:39:37 +0800

    搭建gateway

--- api/src/main/java/com/yao/api/controller/HelloController.java
+++ api/src/main/java/com/yao/api/controller/HelloController.java
@@ -1,7 +1,21 @@
 package com.yao.api.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 测试gateway的一个对外的controller
  */
+@Slf4j
+@RestController
+@RequestMapping("/goods")
+@RequiredArgsConstructor
 public class HelloController {
+    @GetMapping("/find")
+    public String find() {
+        return "hello";
+    }
 }
