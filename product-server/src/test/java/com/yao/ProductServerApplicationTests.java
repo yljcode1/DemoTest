6086d1462ea779f32c5d3c8ba2604d01cb9cd167
yljcode1 <aaa2826455837@gmail.com>
2022-03-31 23:33:22 +0800

    添加订单-产品模块

---
+++ product-server/src/test/java/com/yao/ProductServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
