550814cd6006639bd423ac106f75f8967500fdfe
yljcode1 <aaa2826455837@gmail.com>
2022-04-01 11:51:45 +0800

    order-product连接nacos成功

--- order-server/src/main/java/com/yao/controller/OrderController.java
+++ order-server/src/main/java/com/yao/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.yao.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class OrderController {
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("wi1104wi"));
-    }
+//    public static void main(String[] args) {
+//        System.out.println(new BCryptPasswordEncoder().encode("wi1104wi"));
+//    }
+@RequestMapping("/select")
+public String selectById(String id) {
+    return id;
+}
 }
