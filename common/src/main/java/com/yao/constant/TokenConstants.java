53ac0902c5cce08b791cddaeecb209d12b158778
yljcode1 <aaa2826455837@gmail.com>
2022-04-01 23:08:16 +0800

    gateway鉴权

---
+++ common/src/main/java/com/yao/constant/TokenConstants.java
@@ -0,0 +1,25 @@
+package com.yao.constant;
+
+/**
+ * <p> @description: Token常量 </p>
+ * <p> @date: 2022/4/1 </p>
+ *
+ * @author: yao
+ * @version: 1.0
+ */
+public class TokenConstants {
+    /**
+     * 令牌自定义标识
+     */
+    public static final String AUTHENTICATION = "Authorization";
+
+    /**
+     * 令牌前缀
+     */
+    public static final String PREFIX = "Bearer ";
+
+    /**
+     * 令牌秘钥
+     */
+    public final static String SECRET = "abcdefghijklmnopqrstuvwxyz";
+}
--- gatewayDemo/src/main/java/com/yao/filter/AuthFilter.java
+++ gatewayDemo/src/main/java/com/yao/filter/AuthFilter.java
@@ -1,7 +1,9 @@
 package com.yao.filter;
 
 import com.yao.config.properties.IgnoreWhiteProperties;
+import com.yao.constant.TokenConstants;
 import io.netty.util.internal.StringUtil;
+import jdk.nashorn.internal.parser.Token;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -28,20 +30,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AuthFilter implements GlobalFilter, Ordered {
 
-    /**
-     * 令牌自定义标识
-     */
-    public static final String AUTHENTICATION = "Authorization";
-
-    /**
-     * 令牌前缀
-     */
-    public static final String PREFIX = "Bearer ";
-
-    /**
-     * 令牌秘钥
-     */
-    public final static String SECRET = "abcdefghijklmnopqrstuvwxyz";
 
     private final IgnoreWhiteProperties ignoreWhites;
 
@@ -60,19 +48,26 @@ public class AuthFilter implements GlobalFilter, Ordered {
         // 获取token
         String token = getToken(request);
         if (StringUtil.isNullOrEmpty(token)) {
-
+            return unauthorizedResponse(exchange, "令牌不能为空");
         }
         return null;
     }
 
+    private Mono<Void> unauthorizedResponse(ServerWebExchange exchange, String msg) {
+        log.error("[鉴权异常处理]请求路径:{}", exchange.getRequest().getPath());
+        return null;
+    }
+
     private String getToken(ServerHttpRequest request) {
-        String token = request.getHeaders().getFirst(AUTHENTICATION);
+        String token = request.getHeaders().getFirst(TokenConstants.AUTHENTICATION);
         // 如果前端设置了令牌前缀，则裁剪掉前缀
-        if (StringUtils.hasText(token) && token.startsWith(PREFIX)) {
-            token = token.replaceFirst(PREFIX, "");
+        if (StringUtils.hasText(token) && token.startsWith(TokenConstants.PREFIX)) {
+            token = token.replaceFirst(TokenConstants.PREFIX, "");
         }
         return token;
     }
+
+
 
     private boolean matchers(String url, List<String> whites) {
         if (StringUtil.isNullOrEmpty(url) || CollectionUtils.isEmpty(whites)) {
